f39653c48d1343a9a964a63b33763774b5b31006
8indaas <8indaas@gmail.com>
2014-04-15 09:26:01 -0700

    finished assignment 1

--- r-programming/rprog-data-specdata/corr.R
+++ r-programming/rprog-data-specdata/corr.R
@@ -10,20 +10,15 @@ corr <- function(directory, threshold = 0) {
   ## Return a numeric vector of correlations
   
   nobsDF <- complete(directory)
-  print(class(nobsDF))
-  print(names(nobsDF))
-  print(class(nobsDF$nobs))
-  r <- nobsDF[nobsDF$nobs == threshold,]
-  print(head(r))
-  print(class(r$id))
-  print(r$id)
+  r <- nobsDF[nobsDF$nobs > threshold,]
+  cr <- numeric()
   
   for(i in r$id){
     fileStr <- paste(directory, "/", sprintf("%03d", as.numeric(i)), ".csv", sep = "")
-    print(fileStr)
     myData <- read.csv(fileStr)
-    print(cor(myData$sulfate, myData$nitrate))
+    cr <- append(cr, round(cor(myData$sulfate, myData$nitrate, use="na.or.complete"),5))
   }
+  cr
 }
 
-corr("specData", 200)
+corr("specData", 150)
